1a19416d5c124a804c0bec7a22f2e7d0dfe3460d
Mårten Pettersson <mtnptrsn@gmail.com>
2021-05-04 23:29:07 +0200

    fix increment script bug and android build number

--- scripts/increment.js
+++ scripts/increment.js
@@ -32,31 +32,39 @@ const buildGradleBuffer = fs.readFileSync(buildGradlePath);
 const infoPlistBuffer = fs.readFileSync(infoPlistPath);
 
 const currentAndroidBuildNumber = Number(
-  /versionCode (?<buildNumber>[0-9])/g.exec(buildGradleBuffer.toString()).groups
-    .buildNumber
+  / versionCode (?<buildNumber>[0-9]*)/g.exec(buildGradleBuffer.toString())
+    .groups.buildNumber
 );
+
+console.log({
+  androidBuildNumber,
+  currentAndroidBuildNumber,
+});
 
 const newBuildGradleContents = buildGradleBuffer
   .toString()
-  .replace(/versionName '[0-9]\.[0-9]\.[0-9]'/g, `versionName '${newVersion}'`)
   .replace(
-    /versionCode [0-9]/g,
-    `versionCode ${androidBuildNumber || currentAndroidBuildNumber + 1}`
+    / versionName '[0-9]*\.[0-9]*\.[0-9]*'/g,
+    ` versionName '${newVersion}'`
+  )
+  .replace(
+    / versionCode [0-9]*/g,
+    ` versionCode ${androidBuildNumber || currentAndroidBuildNumber + 1}`
   );
 
 const currentIosBuildNumber = Number(
-  /<string>(?<buildNumber>[0-9])<\/string>/g.exec(infoPlistBuffer.toString())
+  /<string>(?<buildNumber>[0-9]*)<\/string>/g.exec(infoPlistBuffer.toString())
     .groups.buildNumber
 );
 
 const newInfoPlistContents = infoPlistBuffer
   .toString()
   .replace(
-    /<string>[0-9]\.[0-9]\.[0-9]<\/string>/g,
+    /<string>[0-9]*\.[0-9]*\.[0-9]*<\/string>/g,
     `<string>${newVersion}</string>`
   )
   .replace(
-    /<string>[0-9]<\/string>/g,
+    /<string>[0-9]*<\/string>/g,
     `<string>${iosBuildNumber || currentIosBuildNumber + 1}</string>`
   );
 
