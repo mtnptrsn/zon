dc4dc4eac0abb0361a2dab0906087876279879f2
Mårten Pettersson <mtnptrsn@gmail.com>
2022-04-27 17:00:50 +0200

    refactor shuffle

--- packages/socket-server/src/controllers/RoomController.ts
+++ packages/socket-server/src/controllers/RoomController.ts
@@ -62,26 +62,6 @@ export namespace RoomController {
   }
 }
 
-const shuffle = (array: any[]) => {
-  let currentIndex = array.length;
-  let temporaryValue;
-  let randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-};
-
 const checkPointCollected = (
   point: any,
   player: any,
--- packages/socket-server/src/controllers/generateMap.ts
+++ packages/socket-server/src/controllers/generateMap.ts
@@ -1,26 +1,7 @@
 import { getDistance } from "geolib";
+import { shuffle } from "../lib/shuffle";
 import { generateMap } from "../utils/map";
 import { getStreetCoordinates } from "../utils/osm";
-
-const shuffle = (array: any[]) => {
-  let currentIndex = array.length;
-  let temporaryValue;
-  let randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-};
 
 export const getMap = async (homes: [number, number][], radius: number) => {
   return (
@@ -30,18 +11,9 @@ export const getMap = async (homes: [number, number][], radius: number) => {
         const points = generateMap(shuffle(streetCoordinates!), radius, [home]);
 
         const map = points.map((coordinate) => {
-          const distance = getDistance(
-            // could be wrong order!
-            home,
-            coordinate
-          );
-
-          // TODO: Refactor
-          // TODO: Improve this
+          const distance = getDistance(home, coordinate);
           const max = 3;
-          const dp = distance / radius;
-          const randAdd = Math.pow(Math.random(), 2) * dp * max;
-          const weight = Math.max(1, Math.floor(dp * max + randAdd));
+          const weight = Math.max(1, Math.ceil((distance / radius) * max));
 
           return {
             location: {
